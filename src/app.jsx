c29fccd00e040d7a0ce530a5c0f64593c22fa428
Gatis Negribs <gtn@inbox.lv>
2019-01-29 15:54:41 +0200

    first questions

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+require('./mystyles.scss')
 
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: ''};
+    this.state = {
+      age: '',
+      activity: 0
+     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleAge = this.handleAge.bind(this);
+    this.handleActivity = this.handleActivity.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({name: event.target.value});
+  handleAge(event) {
+    const regex = /^[0-9\b]+$/;
+    const maxAge = 100;
+    if ((event.target.value === '' || regex.test(event.target.value)) && event.target.value < maxAge) {
+      this.setState({age: event.target.value})
+    }
+  }
+
+  handleActivity(event) {
+    this.setState({activity: event.target.value})
   }
 
   handleSubmit(event) {
-    alert('Name: ' + this.state.name);
+    if(this.state.age == '') return;
+    if(this.state.activity == 0) return;
+    alert('Age: ' + JSON.stringify(this.state));
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <span>Name:</span>
-          <input type="text" value={this.state.name} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Generate" />
-      </form>
+      <div className="container box">
+        <div className="columns">
+          <div className="column is-3 is-offset-3">
+            <span>Your age:</span>
+          </div>
+          <div className="column is-3">
+            <input type="text" className="input" value={this.state.age} onChange={this.handleAge} />
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column is-3 is-offset-3">
+            <span>How often do you do sports:</span>
+          </div>
+          <div className="column is-3">
+            <span className="select">
+              <select value={this.state.activity} onChange={this.handleActivity}>
+                <option value="0">Never</option>
+                <option value="1">once a month</option>
+                <option value="2">once a week</option>
+                <option value="3">every day</option>
+              </select>
+            </span>
+          </div>
+        </div>
+        <div className="has-text-centered">
+          <a className="button is-primary is-large" onClick={this.handleSubmit}>Calculate</a>
+        </div>
+      </div>
     );
   }
 }
 
-ReactDOM.render(<NameForm />, document.getElementById('app'));
+ReactDOM.render(<NameForm />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require("path")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 const config = {
     mode: "production",
     entry: {
-        js: ["./src/app.jsx"]
+        main: ["./src/app.jsx"]
     },
     output: {
         path: path.join(__dirname, "./dist/"),
@@ -24,6 +25,17 @@ const config = {
                         presets: ['@babel/preset-env', '@babel/react']
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    'sass-loader'
+                    ]
+                })
             }
         ]
     },
@@ -31,9 +43,10 @@ const config = {
         new HtmlWebpackPlugin({
             hash: true,
             template: './src/index.html',
-            chunks: ['js'],
+            chunks: ['main'],
             path: path.join(__dirname, "./dist/")
-        })
+        }),
+        new ExtractTextPlugin('css/mystyles.css')
    ]
 }
 
